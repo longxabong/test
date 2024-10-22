37275a345d28cb3cdd6fba12d62a182600c00996
longxabong <longle23224@gmail.com>
2024-10-23 02:48:45 +0700

    test

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+#region Học phép toán cơ bản
+// Học + - * / %
+// Khai báo biến
+int add = 5 + 10;
+Console.WriteLine($"Kết quả cộng 2 số 5 và 10 là: {add}");
+int minus = 12345 -1234;
+Console.WriteLine($"Kết quả của phép trừ 12345 và 1234 là: {minus}");
+#endregion
